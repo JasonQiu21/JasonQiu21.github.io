eff4c3659cb583aa599185f33e5986771567570f
Jason Qiu <jqiu21@protonmail.com>
2022-08-08 19:26:01 -0400

    changed open mana check to be separate, added color stuff

--- will-glinthorn-kill/glint.js
+++ will-glinthorn-kill/glint.js
@@ -6,8 +6,13 @@ function calculate(){
     let p3 = Number(document.getElementById("p3").value);
     let openMana = Number(document.getElementById("openMana").value);
     let result = document.getElementById("result");
-    if(p1 < 0 || p2 < 0 || p3 < 0 || openMana < 0){
+    if(p1 < 0 || p2 < 0 || p3 < 0){
         result.textContent = "No Negative numbers, please!";
+        result.setAttribute("style", "color: red");
+    } else if(openMana < 2){
+        result.textContent = "You need at least two mana to start the loop.";
+        result.setAttribute("style", "color: red");
+    }
     } else if(openMana + p1 + p2 + p3 - 2*Math.max(p1, p2, p3) >= 1){
         result.textContent = "The combo will kill";
         result.setAttribute("style", "color: green");
